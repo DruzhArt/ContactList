220ffbf3fe318efa16dbd6470b016595941341fb
DruzhArt <druzhart90@gmail.com>
2022-10-22 18:20:10 +0300

    1

--- ContactList/Contacts.swift
+++ ContactList/Contacts.swift
@@ -10,52 +10,11 @@ struct Person {
     let surname: String
     let email: String
     let phone: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
 }
 
-class DataManager {
-    let names: [String] = [
-        "John",
-        "Sharon",
-        "Aaron",
-        "Steven",
-        "Nicola",
-        "Ted",
-        "Bruce",
-        "Carl",
-        "Allan"
-    ]
-    let surnames: [String] = [
-        "Murphy",
-        "Jankin",
-        "Williams",
-        "Black",
-        "White",
-        "Robertson",
-        "Butler",
-        "Smith",
-        "Isaacson"
-    ]
-    let emails: [String] = [
-        "123@gmail.com",
-        "456@gmail.com",
-        "druzhart@mail.ru",
-        "3474@stanki.ru",
-        "3474@rolleri.ru",
-        "987@gmail.com",
-        "jin@gmail.com",
-        "world@gmail.com",
-        "fifa@gmail.com"
-    ]
-    let phones: [String] = [
-        "892167",
-        "999087",
-        "678543",
-        "789456",
-        "123456",
-        "567535",
-        "879837",
-        "565328",
-        "098783"
-    ]
-}
+
 
